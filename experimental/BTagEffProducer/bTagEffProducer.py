d3bbefb96b4887673bdba016232cb1ea379e1e58
Emyr Clement <emyr.clement@bristol.ac.uk>
2014-08-13 16:39:09 +0100

    Add WJets files.  Add efficiency hitsograms combined over both channels.  Add 1D histograms for debugging.

--- experimental/BTagEffProducer/bTagEffProducer.py
+++ experimental/BTagEffProducer/bTagEffProducer.py
@@ -1,18 +1,20 @@
 from rootpy.tree import Tree, TreeChain
-from rootpy.plotting import Hist2D, Canvas
+from rootpy.plotting import Hist, Hist2D, Canvas
 from rootpy.io import root_open
 from rootpy.interactive import wait
 from optparse import OptionParser
 
+from ROOT import gROOT
+gROOT.SetBatch(1)
 
 fileNames = {
              '8TeV' : {
                     'TTJets' : '/storage/TopQuarkGroup/mc/8TeV/BLT/NoBTag/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/TTJets_nTuple_53X_mc_merged.root',
-                    'WJets' : 'WJets8TeV',
+                    'WJets' : '/storage/TopQuarkGroup/mc/8TeV/BLT/NoBTag/WJetsToLNu_TuneZ2Star_8TeV-madgraph-tarball/WJetsToLNu_nTuple_53X_mc_merged.root',
                 },
              '7TeV' : {
                     'TTJets' : '/storage/TopQuarkGroup/mc/7TeV/v11/NoSkimUnfolding/BLT/NoBTag/TTJets_MSDecays_central_TuneZ2_7TeV-madgraph-tauola/TTJets_nTuple_53X_mc_merged.root',
-                     'WJets' : 'WJets8TeV',
+                     'WJets' : '/storage/TopQuarkGroup/mc/7TeV/v11/NoSkimUnfolding/BLT/NoBTag/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/WJetsToLNu_nTuple_53X_mc_merged.root',
                }
              }
 
@@ -32,8 +34,9 @@ jetCollection = {
                  }
 
 # Bin edges that match SFb in 2012
-ptBinEdges = [20, 30, 40, 50, 60, 70, 80, 100, 120, 160, 210, 260, 320, 400, 500, 600,800]
-etaBinEdges = [0,2.4]
+ptBinEdges = [20, 30, 40, 50, 60, 70, 80, 100, 120, 160, 210, 260, 320, 400, 500, 600, 800]
+etaBinEdges = [0,2.6]
+# etaBinEdges = [0,0.8,2.4]
 
 def printEfficienciesToScreen( hist ):
     for xbin in range(1,hist.GetNbinsX() + 1):
@@ -44,6 +47,14 @@ def printEfficienciesToScreen( hist ):
         pass
     pass
 
+def printEfficienciesForAT( hist ):
+    for xbin in range(1,hist.GetNbinsX() + 1):
+        print hist.GetXaxis().GetBinCenter(xbin)
+        for ybin in range(1,hist.GetNbinsY() + 1):    
+            print str(hist.GetBinContent(xbin,ybin))+','
+            pass
+        pass
+    pass
 #===============================================================================
 # Main code
 #===============================================================================
@@ -69,6 +80,19 @@ def main():
         outputFile = 'bTagEfficiencies_'+options.sample+'_8TeV.root'
 
     with root_open(inputFile, 'read') as f, root_open(outputFile, 'recreate') as out:
+        # Combine channel hist
+        # B hists
+        h_bAll_combine = Hist2D(etaBinEdges,ptBinEdges,name='h_bAll_combine')
+        h_bPass_combine = Hist2D(etaBinEdges,ptBinEdges,name='h_bPass_combine')
+                    
+        # C hists
+        h_cAll_combine = Hist2D(etaBinEdges,ptBinEdges,name='h_cAll_combine')
+        h_cPass_combine = Hist2D(etaBinEdges,ptBinEdges,name='h_cPass_combine')
+                    
+        # Light hists
+        h_lAll_combine = Hist2D(etaBinEdges,ptBinEdges,name='h_lAll_combine')
+        h_lPass_combine = Hist2D(etaBinEdges,ptBinEdges,name='h_lPass_combine')
+        
         for channel in channels:
             
             f.cd()
@@ -79,7 +103,7 @@ def main():
             
             # Derive pt variable from tree
             jetPt='sqrt('+jetCollection[channel]+'.Px*'+jetCollection[channel]+'.Px+'+jetCollection[channel]+'.Py*'+jetCollection[channel]+'.Py)'
-            jetEta='-1.0*log(tan(atan(sqrt('+jetCollection[channel]+'.Px*'+jetCollection[channel]+'.Px+'+jetCollection[channel]+'.Py*'+jetCollection[channel]+'.Py)/'+jetCollection[channel]+'.Pz/2)))'
+            jetEta='-1.0*log(tan(atan(sqrt('+jetCollection[channel]+'.Px*'+jetCollection[channel]+'.Px+'+jetCollection[channel]+'.Py*'+jetCollection[channel]+'.Py)/'+jetCollection[channel]+'.Pz)/2))'
             
             # Parton criteria
             bPartonCriteria = 'abs('+jetCollection[channel]+'.PartonFlavour)==5'
@@ -92,40 +116,66 @@ def main():
             # B hists
             h_bAll = Hist2D(etaBinEdges,ptBinEdges,name='h_bAll_'+channel)
             h_bPass = Hist2D(etaBinEdges,ptBinEdges,name='h_bPass_'+channel)
-            
+            h_bAll_1D = Hist(ptBinEdges,name='h_bAll_1D_'+channel)
+            h_bPass_1D = Hist(ptBinEdges,name='h_bPass_1D_'+channel)
+                        
             # C hists
             h_cAll = Hist2D(etaBinEdges,ptBinEdges,name='h_cAll_'+channel)
             h_cPass = Hist2D(etaBinEdges,ptBinEdges,name='h_cPass_'+channel)
-            
+            h_cAll_1D = Hist(ptBinEdges,name='h_cAll_1D_'+channel)
+            h_cPass_1D = Hist(ptBinEdges,name='h_cPass_1D_'+channel)
+                        
             # Light hists
             h_lAll = Hist2D(etaBinEdges,ptBinEdges,name='h_lAll_'+channel)
             h_lPass = Hist2D(etaBinEdges,ptBinEdges,name='h_lPass_'+channel)
+            h_lAll_1D = Hist(ptBinEdges,name='h_lAll_1D_'+channel)
+            h_lPass_1D = Hist(ptBinEdges,name='h_lPass_1D_'+channel)
             
             # Read trees
             # B partons
             tree.Draw(jetEta+':'+jetPt,bPartonCriteria, hist=h_bAll)
             tree.Draw(jetEta+':'+jetPt,bPartonCriteria+'&&'+bTagCriteria, hist=h_bPass)
+            tree.Draw(jetPt,bPartonCriteria+'&& abs('+jetEta+')<2.6', hist=h_bAll_1D)
+            tree.Draw(jetPt,bPartonCriteria+'&& abs('+jetEta+')<2.6'+'&&'+bTagCriteria, hist=h_bPass_1D)
             
             # C partons
             tree.Draw(jetEta+':'+jetPt,cPartonCriteria, hist=h_cAll)
             tree.Draw(jetEta+':'+jetPt,cPartonCriteria+'&&'+bTagCriteria, hist=h_cPass)
-            
+            tree.Draw(jetPt,cPartonCriteria+'&& abs('+jetEta+')<2.6', hist=h_cAll_1D)
+            tree.Draw(jetPt,cPartonCriteria+'&& abs('+jetEta+')<2.6'+'&&'+bTagCriteria, hist=h_cPass_1D)
+                    
             # Light partons
             tree.Draw(jetEta+':'+jetPt,lightPartonCriteria, hist=h_lAll)
             tree.Draw(jetEta+':'+jetPt,lightPartonCriteria+'&&'+bTagCriteria, hist=h_lPass)
-                
+            tree.Draw(jetPt,lightPartonCriteria+'&& abs('+jetEta+')<2.6', hist=h_lAll_1D)
+            tree.Draw(jetPt,lightPartonCriteria+'&& abs('+jetEta+')<2.6'+'&&'+bTagCriteria, hist=h_lPass_1D)
+            
+            # Add to combined histograms
+            h_bAll_combine.Add( h_bAll )
+            h_bPass_combine.Add( h_bPass )
+            h_cAll_combine.Add( h_cAll )
+            h_cPass_combine.Add( h_cPass )
+            h_lAll_combine.Add( h_lAll )
+            h_lPass_combine.Add( h_lPass )
+                        
             # Make efficiency histograms
             h_bPass.Divide(h_bPass,h_bAll,1.,1.,'B')
-            h_bPass.Draw('COLZ')
-            printEfficienciesToScreen(h_bPass)
+            h_bPass_1D.Divide(h_bPass_1D,h_bAll_1D,1.,1.,'B')
+#             h_bPass.Draw('COLZ')
+#             printEfficienciesForAT(h_bPass)
             
             h_cPass.Divide(h_cPass,h_cAll,1.,1.,'B')
-            h_cPass.Draw('COLZ')
-            printEfficienciesToScreen(h_cPass)
-            
+            h_cPass_1D.Divide(h_cPass_1D,h_cAll_1D,1.,1.,'B')
+
+#             h_cPass.Draw('COLZ')
+#             printEfficienciesForAT(h_cPass)
+#             printEfficienciesToScreen(h_cPass)
+
             h_lPass.Divide(h_lPass,h_lAll,1.,1.,'B')
-            h_lPass.Draw('COLZ')
-            printEfficienciesToScreen(h_lPass)
+            h_lPass_1D.Divide(h_lPass_1D,h_lAll_1D,1.,1.,'B')
+#             h_lPass.Draw('COLZ')
+#             printEfficienciesForAT(h_lPass)
+#             printEfficienciesToScreen(h_lPass)
             
             # Output to file
             print 'Writing output'
@@ -133,8 +183,26 @@ def main():
             h_bPass.Write()
             h_cPass.Write()
             h_lPass.Write()
+            h_bPass_1D.Write()
+            h_cPass_1D.Write()
+            h_lPass_1D.Write()
             print 'Done'
             pass
+        
+        # Divide combined histograms
+        h_bPass_combine.Divide(h_bPass_combine,h_bAll_combine,1.,1.,'B')
+        h_cPass_combine.Divide(h_cPass_combine,h_cAll_combine,1.,1.,'B')
+        h_lPass_combine.Divide(h_lPass_combine,h_lAll_combine,1.,1.,'B')
+        
+        print "B Efficiencies"
+        printEfficienciesForAT(h_bPass)
+        print "C Efficiencies"
+        printEfficienciesForAT(h_cPass)
+        print "L Efficiencies"
+        printEfficienciesForAT(h_lPass)
+        h_bPass_combine.Write()
+        h_cPass_combine.Write()
+        h_lPass_combine.Write()
         pass
     pass
 
