341047215ef8029ccc5fc6dc85296ab9068848d5
Emyr Clement <emyr.clement@bristol.ac.uk>
2014-09-26 14:49:19 +0100

    Add mass option for 7TeV samples.

--- experimental/BLTUnfold/runJobsCrab.py
+++ experimental/BLTUnfold/runJobsCrab.py
@@ -33,16 +33,20 @@ jobs = [
         # Scale up/down
         '--is7TeV -s scaleup',
         '--is7TeV -s scaledown',
-                
+
+        # Mass up/down
+        '--is7TeV -s massup',
+        '--is7TeV -s massdown',
+
         # Top pt reweighting
         '--is7TeV --topPtReweighting',
         ]
 
- # Add pdf variations to list of jobs
-for variation in range(1,45):
-    jobs.append('-p %i' % variation)
-    jobs.append('--is7TeV -p %i' % variation)
-    pass
+#  # Add pdf variations to list of jobs
+# for variation in range(1,45):
+#     jobs.append('-p %i' % variation)
+#     jobs.append('--is7TeV -p %i' % variation)
+#     pass
     
 parser = OptionParser()
 parser.add_option('-j','--job_number',type='int',dest='jobNumber',default=0)
